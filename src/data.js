43673944971a0100d2ac3ff852a217dc1ec52f37
pruthvisai122 <64137108+pruthvisai122@users.noreply.github.com>
2022-12-19 22:21:05 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,92 @@
+const data=[
+    {
+        "albumId": 1,
+        "id": 1,
+        "title": "Abhishek sharma",
+        "type": "All-Rounder",
+        "url": "https://images.news18.com/ibnlive/uploads/2021/03/1614590828_abhishek.jpg?im=Resize,width=360,aspect=fit,type=normal?im=Resize,width=320,aspect=fit,type=normal",
+        "thumbnailUrl": "https://images.news18.com/ibnlive/uploads/2021/03/1614590828_abhishek.jpg?im=Resize,width=360,aspect=fit,type=normal?im=Resize,width=320,aspect=fit,type=normal"
+    },
+    {
+        "albumId": 1,
+        "id": 2,
+        "title": "Ben Stokes",
+        "type": "All-Rounder",
+        "url": "https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/288300/288339.6.jpghttps://img.cricketworld.com/images/f-052981/shikhar-dhawan.jpg",
+        "thumbnailUrl": "https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/288300/288339.6.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 3,
+        "title": "Rahul Tripathi",
+        "type": "Batsmen",
+        "url": "https://images.news18.com/ibnlive/uploads/2022/07/will-rahul-tripathi-get-his-debut-cap-in-southampton.jpg",
+        "thumbnailUrl": "https://images.news18.com/ibnlive/uploads/2022/07/will-rahul-tripathi-get-his-debut-cap-in-southampton.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 4,
+        "title": "Aiden Markram",
+        "type": "Batsmen",
+        "url": "https://cf-images.eu-west-1.prod.boltdns.net/v1/static/3588749423001/2acf1324-9ee3-4299-a573-81966451675f/e7faa1e1-60ed-4aae-b459-89c1f88fc68c/1280x720/match/image.jpg",
+        "thumbnailUrl": "https://cf-images.eu-west-1.prod.boltdns.net/v1/static/3588749423001/2acf1324-9ee3-4299-a573-81966451675f/e7faa1e1-60ed-4aae-b459-89c1f88fc68c/1280x720/match/image.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 5,
+        "title": "Pooran",
+        "type": "WK Batsmen",
+        "url": "https://img.cricketnmore.com/uploads/2022/05/watch-nicholas-poorans-fiery-62(34)-against-delhi-capitals-in-a-losing-cause.jpg",
+        "thumbnailUrl": "https://img.cricketnmore.com/uploads/2022/05/watch-nicholas-poorans-fiery-62(34)-against-delhi-capitals-in-a-losing-cause.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 6,
+        "title": "Abdul samad",
+        "type": "Batsmen",
+        "url": "https://www.icccricketschedule.com/wp-content/uploads/2020/01/Abdul-Samad.jpg",
+        "thumbnailUrl": "https://wikibio.in/wp-content/uploads/2020/12/Abdul-Samad-smashes-a-maximum-over-long-on-during-a-match-against-Mumbai-Indians-on-4-October-2020-in-Sharjah.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 7,
+        "title": "Sundar",
+        "type": "All-Rounder",
+        "url": "https://images.news18.com/ibnlive/uploads/2022/04/washington-sundar-ipl.jpg",
+        "thumbnailUrl": "https://images.news18.com/ibnlive/uploads/2022/04/washington-sundar-ipl.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 8,
+        "title": "Marko Jansen",
+        "type": "All-Rounder",
+        "url": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_wide_w_800,q_50/esci/media/motion/2022/0423/dm_220423_INET_CRIC_timeout_m36_srh-bowling_maruti_iqoo_global/dm_220423_INET_CRIC_timeout_m36_srh-bowling_maruti_iqoo_global.jpg",
+        "thumbnailUrl": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_wide_w_800,q_50/esci/media/motion/2022/0423/dm_220423_INET_CRIC_timeout_m36_srh-bowling_maruti_iqoo_global/dm_220423_INET_CRIC_timeout_m36_srh-bowling_maruti_iqoo_global.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 9,
+        "title": "Murugan Ashwin",
+        "type": "Spinner",
+        "url": "https://c.ndtvimg.com/2022-03/804f9edo_murugan-ashwin-bcciipl_625x300_27_March_22.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605",
+        "thumbnailUrl": "https://c.ndtvimg.com/2022-03/804f9edo_murugan-ashwin-bcciipl_625x300_27_March_22.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605"
+    },
+    {
+        "albumId": 1,
+        "id": 10,
+        "title": "Bhuvineshwar kumar",
+        "type": "Pacer",
+        "url": "https://www.insidesport.in/wp-content/uploads/2021/04/2021-04-13-5.jpg",
+        "thumbnailUrl": "https://www.insidesport.in/wp-content/uploads/2021/04/2021-04-13-5.jpg"
+    },
+    {
+        "albumId": 1,
+        "id": 11,
+        "title": "Umran Malik",
+        "type": "Pacer",
+        "url": "https://images.livemint.com/img/2022/04/28/600x338/ANI-20220427276-0_1651122005846_1651122204277.jpg",
+        "thumbnailUrl": "https://images.livemint.com/img/2022/04/28/600x338/ANI-20220427276-0_1651122005846_1651122204277.jpg"
+    },
+]
+
+export default data;
