b98330f53324c21101912aacc9c522d1a9f56f64
pruthvisai122 <64137108+pruthvisai122@users.noreply.github.com>
2022-12-19 22:22:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
+import React from "react";
+import data from "./data";
+
+// import Profile from "./Profile";
+
+const App = () => {
+  return(
+  //   <section className="posts-container">
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //     <Profile/>
+  //   </section>
+  // )
 
 
-function App() {
-  return (
-    <h1>hello</h1>
+    <section className="posts-container">
+      {data.map((eachObj) => {
+        const {id,thumbnailUrl,title,type} = eachObj;
+        return (
+          <Profile 
+            key={id} 
+            id={id} 
+            thumbnailUrl={thumbnailUrl} 
+            title={title} 
+            type={type}
+    
+          />
+        );
+      })}
+    </section>
+  )
+};
+
+const Profile = (props) => {
+  // return (
+  //   <article className="profile-card">
+  //     <img src="https://via.placeholder.com/150/b0f7cc" alt="something"/>
+  //     <h2 className="title">shikar dhawan</h2>
+  //     <a href="https://via.placeholder.com/150/b0f7cc">click here</a>
+  //   </article>
+  // )
+// }
+
+  const {title, thumbnailUrl,type}=props;
+  return(
+      <article className="profile-card">
+          <img src={thumbnailUrl} alt="something"/>
+          <h2>{title}</h2>
+          <p>{type}</p>
+      </article>
   );
 }
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>hello</h1>
+//     </div>
+//   );
+// }
 
 export default App;
